05135bc741facbc2a011b0c4f62232d2f28d350c
Roman Epifanov <roman@softwave.co.il>
2020-08-01 18:06:49 +0300

    hot fix

--- src/api/api-service.ts
+++ src/api/api-service.ts
@@ -4,6 +4,7 @@ import environment from '../environment';
 const _instance = axios.create(
     {
         withCredentials: true,
+        baseURL: 'https://social-network.samuraijs.com/api/1.0/',
         headers: {
             "API-KEY": environment.API_KEY
         }
@@ -11,35 +12,35 @@ const _instance = axios.create(
 );
 
 export const getUsersCall = (page: number): Promise<any> => {
-    return _instance.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}`)
+    return _instance.get(`users?page=${page}`)
         .then((response: any) => {
             return response.data;
         });
 }
 
 export const deleteFollowCall = (userId: number): Promise<any> => {
-    return _instance.delete(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
+    return _instance.delete(`follow/${userId}`)
         .then((response: any) => {
             return response.data;
         });
 }
 
 export const addFollowCall = (userId: number): Promise<any> => {
-    return _instance.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
+    return _instance.post(`follow/${userId}`)
         .then((response: any) => {
             return response.data;
         });
 }
 
 export const getStatusCall = (): Promise<any> => {
-    return _instance.get(`https://social-network.samuraijs.com/api/1.0/auth/me`)
+    return _instance.get(`auth/me`)
         .then((response: any) => {
             return response.data;
         });
 }
 
 export const getProfileCall = (userId: number): Promise<any> => {
-    return _instance.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
+    return _instance.get(`profile/${userId}`)
         .then((response: any) => {
             return response.data;
         });
