858c4aeadb70217b9a3ab9f8fa075edc4a32c0d1
Roman Epifanov <roman@softwave.co.il>
2020-08-03 21:05:15 +0300

    fixes

--- src/components/profile/ProfileAPiContainer.jsx
+++ src/components/profile/ProfileAPiContainer.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Profile from './Profile';
-import { getProfileCall } from '../../api/api-service';
 
 class ProfileAPIContainer extends React.Component {
     componentDidMount() {
