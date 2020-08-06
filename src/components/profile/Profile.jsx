4dddae1fe3104639edbae504d53672f35236ae11
Roman Epifanov <roman@softwave.co.il>
2020-08-06 20:21:58 +0300

    the profile component from func to class

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -3,17 +3,26 @@ import style from './Profile.module.css';
 import PostForm from './components/form-post/PostForm';
 import Post from './components/post/Post';
 import UserProfile from './components/user-profile/UserProfile';
+import { Redirect } from 'react-router-dom';
 
-const Profile = (props) => {
-    return (
-        <main className={style.profile}>
-            <UserProfile profile={props.state.profile} />
+class Profile extends React.Component {
 
-            <PostForm onAddPost={props.onAddPost} onChangePost={(event) => props.onChangePost(event.target.value)} content={props.state.postForm.content}/>
-            
-            {props.state.posts.map((i, key) => <Post key={key} likesCount={i.likesCount} content={i.content}/>)}
-        </main>
-    );
+
+    render() {
+        if (this.props.authState.isNeedLogin) {
+            return <Redirect to={'/login'} />;
+        } else {
+            return (
+                <main className={style.profile}>
+                    <UserProfile profile={this.props.profileState.profile} />
+
+                    <PostForm onAddPost={this.props.onAddPost} onChangePost={(event) => this.props.onChangePost(event.target.value)} content={this.props.profileState.postForm.content} />
+
+                    {this.props.profileState.posts.map((i, key) => <Post key={key} likesCount={i.likesCount} content={i.content} />)}
+                </main>
+            );
+        }
+    }
 }
 
 export default Profile;
