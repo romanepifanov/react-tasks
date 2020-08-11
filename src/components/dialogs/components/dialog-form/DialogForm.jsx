b29ec835dc21cd18e28753dbc0860a7a74700f53
Roman Epifanov <roman@softwave.co.il>
2020-08-11 18:03:03 +0300

    update components with textarea

--- src/components/dialogs/components/dialog-form/DialogForm.jsx
+++ src/components/dialogs/components/dialog-form/DialogForm.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import styles from './DialogForm.module.css';
 import { Button } from 'antd';
 import { Field, reduxForm } from 'redux-form'
+import Textarea from '../../../common/teaxtarea/Textarea';
+import { requeuedField } from '../../../../utils/Validators';
 
 class DialogForm extends React.Component {
 
     render() {
         return (
             <form onSubmit={this.props.handleSubmit} className={styles.form}>
-                <Field id={'email__Id'} className={styles.textarea} name={'content'} component={'textarea'} />
+                <Field id={'email__Id'} name={'content'} component={Textarea} validate={[requeuedField]}/>
 
                 <Button type="primary" htmlType="submit">Submit</Button>
             </form>
--- src/components/profile/components/form-post/PostForm.jsx
+++ src/components/profile/components/form-post/PostForm.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Button } from 'antd';
 import { Field, reduxForm } from 'redux-form'
 import styles from './PostForm.module.css'
+import { requeuedField } from '../../../../utils/Validators';
+import Textarea from '../../../common/teaxtarea/Textarea';
 
 class PostForm extends React.Component {
 
     render() {
         return (
             <form onSubmit={this.props.handleSubmit} className={styles.form}>
-                <Field id={'email__Id'} className={styles.textarea} name={'content'} component={'textarea'} />
+                <Field id={'content__Id'} name={'content'} component={Textarea} validate={[requeuedField]}/>
 
                 <Button type="primary" htmlType="submit">Submit</Button>
             </form>
