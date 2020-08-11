843ba1aa18a3f84badf652554e6586f150913044
Roman Epifanov <roman@softwave.co.il>
2020-08-11 13:30:58 +0300

    update message components

--- src/components/dialogs/components/message/Message.jsx
+++ src/components/dialogs/components/message/Message.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import style from './Message.module.css';
 
-const Message = (props) => {
-    return (
-        <div className={props.own ? style.messageRight : style.messageLeft}>
-            <div className={style.messageBox}>
-                <img className={style.messageLogo} src="" alt=""/>
-                <div  className={style.messageContent}>
-                    <span className={style.messageText}>{props.text}</span>
-                    <span className={style.messageTime}>{props.time}</span>
+class Message extends React.Component {
+
+    getTime = () => {
+       return  this.props.time.toLocaleTimeString('en-US');
+    }
+
+    render() {
+        return (
+            <div className={this.props.own ? style.messageRight : style.messageLeft}>
+                <div className={style.messageBox}>
+                    <img className={style.messageLogo} src="" alt="" />
+                    <div className={style.messageContent}>
+                        <span className={style.messageText}>{this.props.text}</span>
+                        <span className={style.messageTime}>{this.getTime()}</span>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Message;
